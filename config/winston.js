84ebb3e051eeb8b5d86b23fbc358e71d887193ec
Moses igbuku <captainoak@gmail.com>
2018-12-05 17:25:48 +0100

    adjusted logger

--- config/winston.js
+++ config/winston.js
@@ -2,40 +2,40 @@ const winston = require('winston');
 const fs = require('fs');
 const logDir = process.env.LOG_DIR;
 if (!fs.existsSync(logDir)) {
-  fs.mkdir(logDir);
+    fs.mkdir(logDir);
 }
 
 const options = {
-  file: {
-    level: 'info',
-    filename: `${logDir}/info.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false,
-  },
-  console: {
-    level: 'debug',
-    handleExceptions: true,
-    json: false,
-    colorize: true,
-  },
+    file: {
+        level: 'info',
+        filename: `${logDir}/info.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: false,
+    },
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+    },
 };
 
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.File(options.file),
-    new winston.transports.Console(options.console),
-  ],
-  exitOnError: false, // do not exit on handled exceptions
+const logger = new winston.Logger({
+    transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console),
+    ],
+    exitOnError: false, // do not exit on handled exceptions
 });
 
 // create a stream object with a 'write' function that will be used by `morgan`
 logger.stream = {
-  write(message, encoding) {
-    logger.info(message);
-  },
+    write(message, encoding) {
+        logger.info(message);
+    },
 };
 
 module.exports = logger;
