c6173dcb30037cef9bb0610f534d03c25658ce35
Moses igbuku <captainoak@gmail.com>
2019-01-30 13:55:48 +0100

    added admin password to keyvault

--- api/services/userService.js
+++ api/services/userService.js
@@ -5,6 +5,7 @@ const logger = require(`${appRoot}/config/winston`);
 const User = require(`${appRoot}/api/models/userModel`);
 const helperMethods = require(`${appRoot}/api/helpers/helperMethods`);
 const { ROLES } = require(`${appRoot}/api/constants/authConstants`);
+const azureKeyVault = require(`${appRoot}/api/middlewares/authentication/azureKeyVault`);
 
 exports.createAdminUser = async function () {
     try {
@@ -13,16 +14,21 @@ exports.createAdminUser = async function () {
         const userData = fs.readFileSync(`${appRoot}/util/seedData/adminUser.json`);
         const user = JSON.parse(userData);
 
+        const email = JSON.parse(await azureKeyVault.getSecret(process.env.ADMIN_EMAIL, '')).value;
+        const password = JSON.parse(await azureKeyVault.getSecret(process.env.ADMIN_PASSWORD, '')).value;
+
         const dbUser = await User.findOne({
-            email: user.email
+            email
         });
 
         if (!dbUser) {
             logger.info('Creating user');
             user.salt = crypto.randomBytes(16).toString('hex');
-            user.hash = helperMethods.hashPassword(user.salt, user.password);
+            user.hash = helperMethods.hashPassword(user.salt, password);
 
             user.role = [ROLES.ADMIN];
+
+            user.email = email;
 
             await User.create(user);
             logger.info('created user successflly');
