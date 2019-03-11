b928e860f44cd562088ec95731c157d25171551b
Moses Igbuku <m.igbuku@parallelscore.com>
2019-03-11 17:50:55 +0100

    added test for create key

--- spec/unit/middlewares/azureKeyVault.spec.js
+++ spec/unit/middlewares/azureKeyVault.spec.js
@@ -16,9 +16,12 @@ describe.only('AzureKeyVaultHelper', () => {
     let callback;
     let context;
     let tokenResponse;
+    let client;
+    let momentObj;
 
     beforeEach(() => {
         credentials = {};
+        momentObj = {};
         tokenResponse = {
             tokenType: 'bearer',
             accessToken: 'jdbsdadddij9ndwhenj08ehndcsdkhcnjducbwewwcbfuisdwibwinnwidw'
@@ -30,15 +33,19 @@ describe.only('AzureKeyVaultHelper', () => {
             authorization: 'ndnsnifnuoj0enoneneojdddijcidcehnciei20',
             resource: 'bcwhbwebwd9uwhwubeueciwjwjcbcbcdddssdwcwwacss'
         };
+
+        client = {};
         callback = sandbox.spy();
         adalNode = {
             AuthenticationContext: sandbox.stub().returns(context)
         };
         azureKeyVault = {
-            KeyVaultCredentials: sandbox.stub().yields(challenge, callback).returns(credentials)
+            KeyVaultCredentials: sandbox.stub().yields(challenge, callback).returns(credentials),
+            KeyVaultClient: sandbox.stub().returns(client)
         };
 
         const imports = {
+            moment: sandbox.stub().returns(momentObj),
             'adal-node': adalNode,
             'azure-keyvault': azureKeyVault
         };
@@ -60,5 +67,99 @@ describe.only('AzureKeyVaultHelper', () => {
             process.env.CLIENT_ID,
             process.env.CLIENT_SECRET
         );
+
+        assert.calledWith(azureKeyVault.KeyVaultClient, credentials);
+    });
+
+    it('createkey throws error when client.createKey encounters an error', () => {
+        let keyname = 'admin-id';
+        let cb;
+
+        const momentVal = 'few seconds ago';
+        const momentVal1 = '2020-03-11T17:38:08.320';
+
+        momentObj.toNow = sandbox.stub().returns(momentVal);
+        momentObj.add = sandbox.stub().returns(momentVal1);
+
+        const attributes = {
+            enabled: true,
+            notBefore: momentVal,
+            expires: momentVal1
+        };
+        const keyOptions = {
+            keySize: 2048,
+            keyOps: ['encrypt', 'decrypt', 'sign', 'verify', 'wrapKey', 'unwrapKey'],
+            tags: null,
+            keyAttributes: JSON.stringify(attributes)
+        };
+
+        const keyOptionsStr = JSON.stringify(keyOptions);
+
+        const errMsg = 'Unknown connection error';
+        client.createKey = sandbox.stub().yields(new Error(errMsg), null);
+
+        azureKeyVault.KeyVaultClient = sandbox.stub();
+        try {
+            azureKeyVaultHelper.createkey(keyname, cb);
+        } catch (err) {
+            expect(err.message).to.equal(errMsg);
+            assert.threw(client.createKey);
+            assert.calledWith(client.createKey, process.env.VAULT_URI, keyname, 'RSA', keyOptionsStr);
+        }
+    });
+
+    it('createkey throws error when client.createKey encounters an error', () => {
+        let keyname = 'admin-id';
+        let cb = sandbox.spy();
+
+        const result = {
+            id: 'hdsbdjsdubdoeujebwoihnew'
+        };
+
+        const momentVal = 'few seconds ago';
+        const momentVal1 = '2020-03-11T17:38:08.320';
+
+        momentObj.toNow = sandbox.stub().returns(momentVal);
+        momentObj.add = sandbox.stub().returns(momentVal1);
+
+        const attributes = {
+            enabled: true,
+            notBefore: momentVal,
+            expires: momentVal1
+        };
+        const keyOptions = {
+            keySize: 2048,
+            keyOps: ['encrypt', 'decrypt', 'sign', 'verify', 'wrapKey', 'unwrapKey'],
+            tags: null,
+            keyAttributes: JSON.stringify(attributes)
+        };
+
+        const keyOptionsStr = JSON.stringify(keyOptions);
+
+        client.createKey = sandbox.stub().yields(null, result);
+
+        azureKeyVault.KeyVaultClient = sandbox.stub();
+
+        azureKeyVaultHelper.createkey(keyname, cb);
+        assert.calledWith(client.createKey, process.env.VAULT_URI, keyname, 'RSA', keyOptionsStr);
+
+        assert.calledWith(cb, result);
+    });
+
+    it('deleteKey throws error when client.deleteKey encounters an error', () => {
+        let secretName = 'admin-id';
+        let cb;
+
+        const errMsg = 'Unknown connection error';
+        client.deleteKey = sandbox.stub().yields(new Error(errMsg), null);
+
+        azureKeyVault.KeyVaultClient = sandbox.stub();
+        try {
+            azureKeyVaultHelper.deletekey(secretName, cb);
+        } catch (err) {
+            expect(err.message).to.equal(errMsg);
+            assert.threw(client.deleteKey);
+            assert.calledWith(client.deleteKey, process.env.VAULT_URI, secretName);
+        }
     });
 });
